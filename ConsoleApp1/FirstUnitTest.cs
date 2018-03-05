1291738578927986599d79a4d9c6ea149b8e16e1
Risto Päärni <risto.paarni@lifetime.fi>
2018-03-05 15:21:54 +0200

    Add files via upload

---
+++ ConsoleApp1/FirstUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitReview
+
+{
+    public class FirstUnitTest
+    {
+        //* [Fact] means a unit test
+        [Fact]
+        public void Assert_Account_Addition()
+        {
+            var account = new Account(8);
+            account.AddBalance(1);
+
+            Assert.Equal(9, account.Balance);
+        }
+
+        [Fact]
+        public void Assert_Person_Equality()
+        {
+            var person1 = new Person()
+            {
+                FirstName = "Päärni",
+                LastName = "Risto"
+            };
+            var person2 = new Person()
+            {
+                FirstName = "Päärni",
+                LastName = "Risto"
+            };
+            
+            
+            ;
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Assert.NotSame(person2, person1);
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Person
+    { public string FirstName;
+      public string LastName;
+
+        public override bool Equals(object obj)
+        {
+            return true;
+        }
+    }
+
+    public class Account
+    {
+        public Account(decimal initialBalance)
+        {
+            this.Balance = initialBalance;
+        }
+        public decimal Balance { get; set; }
+
+        public void AddBalance(decimal value)
+        {
+            this.Balance += value;
+        }
+    }
+    
+    
+}
